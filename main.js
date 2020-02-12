1e1c465ff2f63bb5110a8fb977ba8663a00d8086
Carlos Eduardo Bussola Mancini <cadumancini@gmail.com>
2020-02-12 17:04:20 -0300

    Configuração do Babel

---
+++ main.js
@@ -0,0 +1,10 @@
+alert('Teste'); // funciona em qualquer browser, não vai converter nada
+
+class Teste { // como não existe class em browsers mais antigos, faz a conversão para a forma que funciona (ver bundle.js)
+  metodo() {
+
+  }
+  outro() {
+    
+  }
+}
