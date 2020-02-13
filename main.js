fee87e2d7503803e424b834c15eb6591c32a4698
Carlos Eduardo Bussola Mancini <cadumancini@gmail.com>
2020-02-13 11:56:34 -0300

    Classes

--- main.js
+++ main.js
@@ -1,8 +1,53 @@
-class Teste { // como não existe class em browsers mais antigos, faz a conversão para a forma que funciona (ver bundle.js)
-  metodo() {
-
+class List {
+  constructor() {
+    this.data = [];
   }
-  outro() {
-    
+
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
   }
 }
+
+class TodoList extends List{
+  constructor() {
+    super(); // chamando constructor() da classe pai
+    this.usuario = 'Cadu';
+  }
+
+  mostraUsuario() {
+    console.log(this.usuario);
+  }
+}
+
+var MinhaLista = new TodoList();
+document.getElementById('novoTodo').onclick = function() {
+  MinhaLista.add('Novo ToDo');
+}
+
+MinhaLista.mostraUsuario();
+
+/* ---------------------------------------------------------- */
+// Métodos static
+
+class TodoList2 {
+  constructor() {
+    this.todos = [];
+  }
+
+  static addTodo() {
+    this.todos.push('Novo ToDo');
+    console.log(this.todos);
+  }
+}
+
+// TodoList2.addTodo(); // não funciona, pois método static não enxerga o 'this'. Serve para receber informação, processar e devolver
+// classe que tem static geralmente não tem contrutor
+
+class Matematica {
+  static soma(a, b) {
+    return a + b;
+  }
+}
+
+console.log(Matematica.soma(1, 2));
