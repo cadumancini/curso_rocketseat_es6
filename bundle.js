3313c9ea43c546fb36e4f9d12a82de506e792377
cadumancini <cadumancini@gmail.com>
2020-02-15 08:18:45 -0300

    Operadores REST/SPREAD

--- bundle.js
+++ bundle.js
@@ -1,31 +1,78 @@
 "use strict";
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+// REST operator:
 var usuario = {
   nome: 'Cadu',
   idade: 27,
-  endereco: {
-    cidade: 'Matão',
-    uf: 'SP'
-  }
-}; // forma padrão:
-//const nome = usuario.nome;
-//const idade = usuario.idade;
-//const cidade = usuario.endereco.cidade;
-// usando desestruturação:
+  cidade: 'Matão'
+};
 
 var nome = usuario.nome,
-    idade = usuario.idade,
-    cidade = usuario.endereco.cidade; // aliementa as três variáveis
+    resto = _objectWithoutProperties(usuario, ["nome"]); // usando '...<variavel>' será atribuído o resto do objeto que não foi atribuido para a primeira variável
+
 
 console.log(nome);
-console.log(idade);
-console.log(cidade);
+console.log(resto); // fazendo com arrays:
 
-function mostraNomeIdade(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  // pode-se passar os atributos do objeto como um novo objeto no parâmetro da função
-  console.log(nome, idade);
+var arr = [1, 2, 3, 4];
+var a = arr[0],
+    b = arr[1],
+    c = arr.slice(2);
+console.log(a); // a = 1
+
+console.log(b); // b = 2
+
+console.log(c); // c = [3, 4]
+// pode-se usar como parâmetro de função
+
+function soma() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
 }
 
-mostraNomeIdade(usuario);
+console.log(soma(1, 2, 3));
+
+function teste(a, b) {
+  for (var _len2 = arguments.length, params = new Array(_len2 > 2 ? _len2 - 2 : 0), _key2 = 2; _key2 < _len2; _key2++) {
+    params[_key2 - 2] = arguments[_key2];
+  }
+
+  // atribui os dois primeiros parametros para a e b, e o resto vai para dentro do params
+  return params;
+}
+
+console.log(teste(1, 2, 3, 4, 5, 6)); // SPREAD operator:
+
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var arr3 = [].concat(arr1, arr2); // pega os dados de arr1 e junta com os de arr2;
+
+console.log(arr3); //outra forma de usar (com objetos):
+
+var usuario1 = {
+  nome: 'Cadu',
+  idade: 27,
+  cidade: 'Matão'
+};
+
+var usuario2 = _objectSpread({}, usuario1, {
+  cidade: 'Vancouver'
+}); // atribui todos os atributos do usuario1 para o usuario2, e pode-se trocar apenas o que for necessário
+
+
+console.log(usuario2);
--- main.js
+++ main.js
@@ -1,25 +1,45 @@
+// REST operator:
 const usuario = {
   nome: 'Cadu',
   idade: 27,
-  endereco: {
-    cidade: 'Matão',
-    uf: 'SP'
-  },
-};
-
-// forma padrão:
-//const nome = usuario.nome;
-//const idade = usuario.idade;
-//const cidade = usuario.endereco.cidade;
-
-// usando desestruturação:
-const { nome, idade, endereco: { cidade } } = usuario; // aliementa as três variáveis
-console.log(nome);
-console.log(idade);
-console.log(cidade);
-
-function mostraNomeIdade({nome, idade}) { // pode-se passar os atributos do objeto como um novo objeto no parâmetro da função
-  console.log(nome, idade);
+  cidade: 'Matão'
 }
 
-mostraNomeIdade(usuario);
+const {nome, ...resto} = usuario; // usando '...<variavel>' será atribuído o resto do objeto que não foi atribuido para a primeira variável
+console.log(nome);
+console.log(resto);
+
+// fazendo com arrays:
+const arr = [1, 2, 3, 4];
+const [a, b, ...c] = arr;
+
+console.log(a); // a = 1
+console.log(b); // b = 2
+console.log(c); // c = [3, 4]
+
+// pode-se usar como parâmetro de função
+function soma(...params) {
+  return params.reduce((total, next) => total + next);
+}
+console.log(soma(1, 2, 3));
+
+function teste(a, b, ...params){ // atribui os dois primeiros parametros para a e b, e o resto vai para dentro do params
+  return params;
+}
+console.log(teste(1, 2, 3, 4, 5, 6));
+
+// SPREAD operator:
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = [...arr1, ...arr2]; // pega os dados de arr1 e junta com os de arr2;
+console.log(arr3);
+
+//outra forma de usar (com objetos):
+const usuario1 = {
+  nome: 'Cadu',
+  idade: 27,
+  cidade: 'Matão'
+};
+
+const usuario2 = {...usuario1, cidade: 'Vancouver'}; // atribui todos os atributos do usuario1 para o usuario2, e pode-se trocar apenas o que for necessário
+console.log(usuario2);
