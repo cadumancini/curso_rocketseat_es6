270fc862d5a6d1e443cf0f96fde06667a13d4694
Carlos Eduardo Bussola Mancini <cadumancini@gmail.com>
2020-02-14 21:32:09 -0300

    Operações em Array

--- bundle.js
+++ bundle.js
@@ -1,22 +1,24 @@
 "use strict";
 
-var a = 1; // a = 3; // não funciona, pois é constante (read-only). não pode reatribuir valor
-
-var usuario = {
-  nome: 'Cadu'
-};
-usuario.nome = 'Eduardo'; // mutação funciona, o que é diferente de reatribuição
-
-console.log(usuario);
-/* ------------------------------------------------ */
-
-function teste(x) {
-  var y = 2;
-
-  if (x > 5) {
-    console.log(x, y); // aqui ele enxerga o y
-  }
-} // console.log(y); não funciona, pois variáveis do tipo 'let' são de escopo
-
-
-teste(10);
+var arr = [1, 2, 4, 5, 8, 9, 12];
+var newArr = arr.map(function (item, index) {
+  // a variável da função recebe cada item do array mapeado
+  return item * 2; // e retorna ele multiplicado por 2 pra dentro do novo array, neste caso.
+  // return item + index; // também poderia ser dessa forma, pois também podemos utilizar o índice do array
+});
+console.log(newArr);
+var sum = arr.reduce(function (total, next) {
+  // variáveis para trabalhar a cada iteração, sendo 'next' o item iterado
+  return total + next; // vai somando cada item
+});
+console.log(sum);
+var filter = arr.filter(function (item) {
+  // o filter faz com que cada item fique ou não no novo array se a condição da função for true
+  return item % 2 === 0; // deixando ficar no array apenas os pares.
+});
+console.log(filter);
+var find = arr.find(function (item) {
+  // procura dentro do array e retorna os que retornarem true dentro da função
+  return item === 4; // procurando itens igual a 4. se não encontrar, vai retornar 'undefined'.
+});
+console.log(find);
