041c1a5f8fc98c8b6d5ce8a006952e8e2da419ec
cadumancini <cadumancini@gmail.com>
2020-02-15 07:55:26 -0300

    Valore s padrão

--- bundle.js
+++ bundle.js
@@ -1,47 +1,11 @@
 "use strict";
 
-var arr = [1, 3, 4, 5, 6];
-var newArr = arr.map(function (item) {
-  // Arrow function. não precisa usar o nome function (por ser anônima), e após o parâmetro, usar '=>'
-  return item * 2;
-});
-console.log(newArr);
-var newArr2 = arr.map(function (item) {
-  // Outra forma: quando tem apenas 1 parâmetro, não precisa do '(param)'
-  return item * 2;
-});
-console.log(newArr2);
-var newArr3 = arr.map(function (item) {
-  return item * 2;
-}); // Outra forma: quando tem apenas o return <código>, não precisa do '{ return ... }'
-
-console.log(newArr3);
-
-var teste = function teste() {
-  // também é possível criar funções arrow dessa forma, porém recomenda-se usar 'function'
-  return 'teste';
+var soma = function soma() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
 };
 
-console.log(teste());
+console.log(soma(1)); // como foi passado apenas 1 parâmetro, o JS entende como sendo o primeiro, e atribui o valor padrâo para b.
 
-var teste2 = function teste2() {
-  return 'teste';
-}; // assim também funciona
-
-
-console.log(teste2());
-
-var retornaArr = function retornaArr() {
-  return [1, 2, 3];
-};
-
-console.log(retornaArr());
-
-var retornaObj = function retornaObj() {
-  return {
-    nome: 'Cadu'
-  };
-}; // para retornar objeto, deve-se usar parênteses em volta das chaves
-
-
-console.log(retornaObj());
+console.log(soma()); // usa o valor padrâo para os dois parâmetros
--- main.js
+++ main.js
@@ -1,27 +1,4 @@
-const arr = [1, 3, 4, 5, 6];
-const newArr = arr.map((item) => { // Arrow function. não precisa usar o nome function (por ser anônima), e após o parâmetro, usar '=>'
-  return item * 2;
-});
-console.log(newArr);
+const soma = (a = 3, b = 6) => a + b;
 
-const newArr2 = arr.map(item => { // Outra forma: quando tem apenas 1 parâmetro, não precisa do '(param)'
-  return item * 2;
-});
-console.log(newArr2);
-
-const newArr3 = arr.map(item => item * 2); // Outra forma: quando tem apenas o return <código>, não precisa do '{ return ... }'
-console.log(newArr3);
-
-const teste = () => { // também é possível criar funções arrow dessa forma, porém recomenda-se usar 'function'
-  return 'teste';
-}
-console.log(teste());
-
-const teste2 = () => 'teste'; // assim também funciona
-console.log(teste2());
-
-const retornaArr = () => [1, 2, 3];
-console.log(retornaArr());
-
-const retornaObj = () => ({ nome: 'Cadu' }); // para retornar objeto, deve-se usar parênteses em volta das chaves
-console.log(retornaObj()); 
+console.log(soma(1)); // como foi passado apenas 1 parâmetro, o JS entende como sendo o primeiro, e atribui o valor padrâo para b.
+console.log(soma()); // usa o valor padrâo para os dois parâmetros
