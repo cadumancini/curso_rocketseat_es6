6414c13cfa6eb1c6f2fadc3d6962fe580582db5c
Carlos Eduardo Bussola Mancini <cadumancini@gmail.com>
2020-02-19 14:55:12 -0300

    Exercício 03 do Módulo 01

--- Exercicios_01/bundle.js
+++ Exercicios_01/bundle.js
@@ -96,11 +96,53 @@ var google = usuarios.find(function (item) {
 console.log('Trabalham no Google: ', google);
 
 for (var _i = 0, _usuarios = usuarios; _i < _usuarios.length; _i++) {
-  var usuario = _usuarios[_i];
-  usuario.idade *= 2;
+  var _usuario = _usuarios[_i];
+  _usuario.idade *= 2;
 }
 
 var rocketseatAte50 = usuarios.filter(function (item) {
   return item.idade <= 50;
 });
-console.log(rocketseatAte50);
+console.log(rocketseatAte50); // Exercicio 03:
+
+console.log('---------------------------------------------------------');
+console.log('Exercício 03:'); // 3.1
+
+var arr = [1, 2, 3, 4, 5];
+arr.map(function (item) {
+  return item + 10;
+}); // 3.2
+
+var usuario = {
+  nome: 'Diego',
+  idade: 23
+};
+
+var mostraIdade = function mostraIdade(usuario) {
+  return usuario.idade;
+};
+
+console.log(mostraIdade(usuario)); // 3.3
+
+var nome = "Diego";
+var idade = 23;
+
+var mostraUsuario = function mostraUsuario() {
+  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Diego';
+  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
+  return {
+    nome: nome,
+    idade: idade
+  };
+};
+
+console.log(mostraUsuario(nome));
+console.log(mostraUsuario(nome, idade)); // 3.4
+
+var promise = function promise() {
+  return new Promise(function (resolve, reject) {
+    return resolve();
+  });
+};
+
+console.log(promise());
--- Exercicios_01/main.js
+++ Exercicios_01/main.js
@@ -56,3 +56,26 @@ const rocketseatAte50 = usuarios.filter(function(item) {
   return item.idade <= 50;
 });
 console.log(rocketseatAte50);
+
+// Exercicio 03:
+console.log('---------------------------------------------------------');
+console.log('Exercício 03:');
+// 3.1
+const arr = [1, 2, 3, 4, 5];
+arr.map((item) => item + 10);
+
+// 3.2
+const usuario = { nome: 'Diego', idade: 23 };
+const mostraIdade = (usuario) => usuario.idade;
+console.log(mostraIdade(usuario));
+
+// 3.3
+const nome = "Diego";
+const idade = 23;
+const mostraUsuario = (nome = 'Diego', idade = 18) => ({nome, idade});
+console.log(mostraUsuario(nome));
+console.log(mostraUsuario(nome, idade));
+
+// 3.4
+const promise = () => new Promise((resolve, reject) => resolve());
+console.log(promise());
