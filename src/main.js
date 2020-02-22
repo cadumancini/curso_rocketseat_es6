459793c5880808e12873a60140a66bf607c958d9
cadumancini <cadumancini@gmail.com>
2020-02-22 10:16:18 -0300

    Async/await

--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
-// Exercícios Módulo 02:
-import ClasseUsuario, { idade as IdadeUsuario } from './functions';
-// 1.1
-ClasseUsuario.info();
+const minhaPromise = () => new Promise((resolve, reject) => {
+  setTimeout(() => {resolve('OK')}, 2000);
+});
 
-// 1.2
-//console.log(idade);
+// antes era feito da seguinte forma:
+// minhaPromise()
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(err => {
 
-// 1.3
-console.log(IdadeUsuario);
+//   });
+
+// agora é feito assim:
+// ao definir uma função como async, ela automaticamente cria uma Promise
+async function executaPromise(){
+  //const response = await minhaPromise();
+  //console.log(response);
+
+  // o await faz com que a próxima linha só execute quando a primeira acabar. assim não precisa ficar chamando then/catch toda vez.
+  // não é possível usar await fora de uma função async
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+}
+executaPromise();
+
+// pode-se usar também como arrow function:
+const executaPromise2 = async () => {
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+};
+executaPromise2();
