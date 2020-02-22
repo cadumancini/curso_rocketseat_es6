bceb99f9d46b4ce53ec53c06b3024bcef84a9c61
cadumancini <cadumancini@gmail.com>
2020-02-22 10:24:07 -0300

    Configurando axios

--- src/main.js
+++ src/main.js
@@ -1,34 +1,16 @@
-const minhaPromise = () => new Promise((resolve, reject) => {
-  setTimeout(() => {resolve('OK')}, 2000);
-});
+import axios from 'axios';
 
-// antes era feito da seguinte forma:
-// minhaPromise()
-//   .then(response => {
-//     console.log(response);
-//   })
-//   .catch(err => {
-
-//   });
-
-// agora é feito assim:
-// ao definir uma função como async, ela automaticamente cria uma Promise
-async function executaPromise(){
-  //const response = await minhaPromise();
-  //console.log(response);
-
-  // o await faz com que a próxima linha só execute quando a primeira acabar. assim não precisa ficar chamando then/catch toda vez.
-  // não é possível usar await fora de uma função async
-  console.log(await minhaPromise());
-  console.log(await minhaPromise());
-  console.log(await minhaPromise());
+class Api {
+  static async getUserInfo(username) {
+    // o await não tem a função de then/catch, tem que usar o try/catch:
+    try {
+      const response = await axios.get(`https://api.github.com/users/${username}`);
+      console.log(response);
+    } catch (err) {
+      console.warn('Erro na API: ', err.message);
+    }
+  }
 }
-executaPromise();
 
-// pode-se usar também como arrow function:
-const executaPromise2 = async () => {
-  console.log(await minhaPromise());
-  console.log(await minhaPromise());
-  console.log(await minhaPromise());
-};
-executaPromise2();
+Api.getUserInfo('cadumancini'); // funciona OK
+Api.getUserInfo('cadumancini1225345321qrq'); // retorna 404
