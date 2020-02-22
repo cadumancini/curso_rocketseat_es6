0cdb04a331865ae481154256bd7e28b7b6ddb2ce
cadumancini <cadumancini@gmail.com>
2020-02-22 10:48:35 -0300

    Exercícios Módulo 03

--- src/main.js
+++ src/main.js
@@ -1,16 +1,52 @@
-import axios from 'axios';
+// Exercícios Módulo 03:
 
-class Api {
-  static async getUserInfo(username) {
-    // o await não tem a função de then/catch, tem que usar o try/catch:
+// 3.1
+const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+async function umPorSegundo(){
+  await delay();
+  console.log('1s');
+  await delay();
+  console.log('2s');
+  await delay();
+  console.log('3s');
+}
+umPorSegundo();
+
+// 3.2
+import axios from 'axios';
+async function getUserFromGitHub(user) {
+  try{
+    const response = await axios.get(`https://api.github.com/users/${user}`);
+    console.log(response);
+  } catch (err) {
+    console.log('Usuário não existe!');
+  }
+}
+getUserFromGitHub('cadumancini');
+getUserFromGitHub('cadumancinisadfsfasefa12');
+
+// 3.3
+class Github {
+  static async getRepositories(repo) {
     try {
-      const response = await axios.get(`https://api.github.com/users/${username}`);
-      console.log(response);
+      const response = await axios.get(`https://api.github.com/repos/${repo}`);
+      console.log(response.data);
     } catch (err) {
-      console.warn('Erro na API: ', err.message);
+      console.log('Repositório não existe');
     }
   }
 }
+Github.getRepositories('cadumancini/Unique');
+Github.getRepositories('cadumancini/Unique1245443');
 
-Api.getUserInfo('cadumancini'); // funciona OK
-Api.getUserInfo('cadumancini1225345321qrq'); // retorna 404
+// 3.4
+const buscaUsuario = async usuario => {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${usuario}`);
+    console.log(response);   
+  }
+  catch (err) { 
+    console.log('Usuário não existe!');
+  }
+}
+buscaUsuario('cadumancini');
